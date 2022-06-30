b7c05d376d6803394f09ef227783f17419ee3ab9
Sreedhar Lakshmanan <sreedhar.lakshmanan@non.se.com>
2022-06-30 11:49:06 +0530

    Added Useralam Row design.

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -11,6 +11,7 @@ import { BasicSettingComponent } from './basic-setting/basic-setting.component';
 import { SettingsComponent } from './settings/settings.component';
 import { DiagnoseTabComponent } from './diagnose-tab/diagnose-tab.component';
 import { ProtectionTabComponent } from './protection-tab/protection-tab.component';
+import { UserAlamComponent } from './user-alam/user-alam.component';
 
 @NgModule({
   imports: [
@@ -19,6 +20,6 @@ import { ProtectionTabComponent } from './protection-tab/protection-tab.componen
     IonicModule,
     FolderPageRoutingModule
   ],
-  declarations: [FolderPage,BasicSettingComponent,SettingsComponent,DiagnoseTabComponent,ProtectionTabComponent]
+  declarations: [FolderPage,BasicSettingComponent,SettingsComponent,DiagnoseTabComponent,ProtectionTabComponent,UserAlamComponent]
 })
 export class FolderPageModule {}
