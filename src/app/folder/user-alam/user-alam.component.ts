626db5211fa1fe0fb3c4b44eba61ce832aec386e
Sreedhar Lakshmanan <sreedhar.lakshmanan@non.se.com>
2022-07-01 16:10:24 +0530

    SettingsUI and Diagnose UI completed.

--- src/app/folder/user-alam/user-alam.component.ts
+++ src/app/folder/user-alam/user-alam.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-alam.component.scss'],
 })
 export class UserAlamComponent implements OnInit {
- lists: Array<Number> = [1,2,3,4,5,6,7,8]
+ lists: Array<Number> = [1,2,3,4,5,6,7,8,9,10]
   constructor() { }
 
   ngOnInit() {}
