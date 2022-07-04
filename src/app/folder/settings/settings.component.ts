37de62c4dcfca10a437cdaad39bc01b672e6c5ef
Sreedhar Lakshmanan <sreedhar.lakshmanan@non.se.com>
2022-07-04 20:00:00 +0530

    Settings - controlSignal List UI InProgress.

--- src/app/folder/settings/settings.component.ts
+++ src/app/folder/settings/settings.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit { 
-
+  lists: Array<string> = ["A",'AB','AC','AD','B-Ht','H-HT','L-Lt','AC','AD','B-Ht','H-HT','L-Lt']
   constructor() { }
 
   ngOnInit() {}
-
+  slideOpts = {
+    initialSlide: 0,
+    speed: 500
+};
 }
