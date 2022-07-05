05556844e0a7c34dd1c032ac62d5397d768b1e3c
Sreedhar Lakshmanan <sreedhar.lakshmanan@non.se.com>
2022-07-05 17:06:40 +0530

    Diagnose Tab UI completed.

--- src/app/folder/settings/settings.component.ts
+++ src/app/folder/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -6,12 +7,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit { 
-  lists: Array<string> = ["A",'AB','AC','AD','B-Ht','H-HT','L-Lt','AC','AD','B-Ht','H-HT','L-Lt']
-  constructor() { }
+  selectedSignal = 'A-AT'
+ lists: Array<string> = ["A",'AB','AC','AD','B-Ht','L-Lt']
+  constructor(private pickerCtrl: PickerController) { }
 
   ngOnInit() {}
-  slideOpts = {
-    initialSlide: 0,
-    speed: 500
-};
+  
+  onClick(){
+console.log('Success')
+  }
+
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'ControlSignal',
+          options: [
+            {
+              text: 'A-At',
+              value: 'A-At',
+            },
+            {
+              text: 'Ac',
+              value: 'Ac',
+            },
+            {
+              text: 'Ad',
+              value: 'Ad',
+            },
+            {
+              text: 'Ah',
+              value: 'Ah',
+            },
+            {
+              text: 'Ak',
+              value: 'Ak',
+            },
+            {
+              text: 'B',
+              value: 'B',
+            },
+            {
+              text: 'Bw',
+              value: 'Bw',
+            },
+            {
+              text: 'C',
+              value: 'C',
+            },
+            {
+              text: 'D-Dt',
+              value: 'D-Dt',
+            },
+            {
+              text: 'Di-Dit',
+              value: 'Di-Dit',
+            },
+            {
+              text: 'H-Ht',
+              value: 'H-Ht',
+            },
+            {
+              text: 'L-Lt',
+              value: 'L-Lt',
+            },
+            {
+              text: 'Li-Lit',
+              value: 'Li-Lit',
+            },
+            {
+              text: 'N',
+              value: 'N',
+            },
+            {
+              text: 'O',
+              value: 'O',
+            },
+            {
+              text: 'P-Pt',
+              value: 'P-Pt',
+            },
+            {
+              text: 'Tl',
+              value: 'Tl',
+            },
+            {
+              text: 'Tt',
+              value: 'Tt',
+            },
+            {
+              text: 'W',
+              value: 'W',
+            },
+            {
+              text: 'Qt-Qtt',
+              value: 'Qt-Qtt',
+            },
+
+          ],
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            window.alert(`You selected: ${value.ControlSignal.value}`);
+            this.selectedSignal = value.ControlSignal.value
+          },
+        },
+      ],
+    });
+
+    await picker.present();
+  }
 }
